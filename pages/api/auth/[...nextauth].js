bec00927c62cd7e0fc49446eb0aac978fbf7e861
Joshua Mbogo <55801923+mbogo-mit@users.noreply.github.com>
2023-08-11 12:39:31 -0400

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,5 +11,5 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || publicRuntimeConfig.GOOGLE_CLIENT_SECRET,
     })
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET || publicRuntimeConfig.JWT_SECRET,
 });
