1310f8fb3becc5cc9f06829274cfa750647053fb
Joshua Mbogo <55801923+mbogo-mit@users.noreply.github.com>
2023-08-11 12:26:22 -0400

    Revert "Update index.jsx"
    This reverts commit 92845330f55c107c1b1f3a46f0c6b4953502f6f6.

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,6 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 import '../src/app/assets/css/main.css';
 // import '../src/app/assets/css/noscript.css';
 
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig();
-
 export default function Home() {
 
 	const { data: session } = useSession();
@@ -193,7 +189,7 @@ export default function Home() {
 									<button
 										className="button fit primary"
 										onClick={() => signIn('google', { callbackUrl: '/projects' })}
-									>{session ? 'Open Projects' : publicRuntimeConfig.NEXT_AUTH_URL }</button>
+									>{session ? 'Open Projects' : 'Sign In'}</button>
 								</li>
 								{/* <li><a href="#" className="button fit">Learn More</a></li> */}
 							</ul>
