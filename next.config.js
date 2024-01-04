bb1ba426023156ed092dc77179951ba13e5a6870
Joshua Mbogo <55801923+mbogo-mit@users.noreply.github.com>
2024-01-04 16:58:12 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     publicRuntimeConfig: {
-      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-      JWT_SECRET: process.env.JWT_SECRET,
-      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-      WEBHUB_HOST: process.env.WEBHUB_HOST,
-      MONGODB_API_KEY: process.env.MONGODB_API_KEY,
+      GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      JWT_SECRET: process.env.NEXT_PUBLIC_JWT_SECRET,
+      NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+      WEBHUB_HOST: process.env.NEXT_PUBLIC_WEBHUB_HOST,
+      MONGODB_API_KEY: process.env.NEXT_PUBLIC_MONGODB_API_KEY,
       // WEBHUB_DB_URL: process.env.WEBHUB_DB_URL,
     },
   };
