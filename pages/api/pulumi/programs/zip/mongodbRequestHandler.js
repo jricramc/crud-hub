afd23a970d75235d5df9a19c1731424bed5500c3
Joshua Mbogo <55801923+mbogo-mit@users.noreply.github.com>
2024-01-04 15:20:23 -0500

    updates

--- pages/api/pulumi/programs/zip/mongodbRequestHandler.js
+++ pages/api/pulumi/programs/zip/mongodbRequestHandler.js
@@ -1,25 +1,52 @@
 const https = require('https');
 
-const parseBody = (body, isBase64Encoded) => {
-    if (!body) { 
-        return {};
+const isBase64 = (str) => {
+    try {
+        // Check if decoded string is the same as the original string. 
+        // If so, it's likely not a valid base64 encoded string.
+        return Buffer.from(str, 'base64').toString('base64') === str;
+    } catch (e) {
+        return false;
+    }
+};
+
+const parseBody = (body) => {
+    if (!body) {
+        return
     }
 
-    if (isBase64Encoded) {
-        const decodedString = Buffer.from(body, 'base64').toString('utf8');
-        try {
-            return JSON.parse(decodedString);
-        } catch(err) {
-            console.error("Error parsing base64 encoded body:", err);
-            return {};
-        }
+    const type = typeof(body);
+    if (type === 'object') {
+        return body;
     }
+    
 
     try {
-        return JSON.parse(body);
+        if (isBase64(body)) {
+            const decodedBase64 = Buffer.from(body, 'base64').toString('utf8');
+            return JSON.parse(decodedBase64);
+         }
+        // stringified JSON
+        return JSON.parse(body)
     } catch (err) {
-        console.error("Error parsing JSON:", err);
-        return {};
+         
+        
+
+        // url encoded
+        const decodedString = Buffer.from(body, 'base64').toString('utf8');
+            
+        const inputString = decodedString
+        
+        // Splitting by '&' to get key-value pairs
+        const keyValuePairs = inputString.split('&').map(pair => pair.split('='));
+                
+        // Convert 2D array to object and decode each URL encoding value 
+        const resultObject = keyValuePairs.reduce((obj, [key, value]) => {
+            obj[key] = decodeURIComponent(value);
+            return obj;
+        }, {});
+
+        return resultObject;
     }
 };
 
@@ -65,7 +92,7 @@ const requestMongoDBPostRequest = (dbname, requestData) => {
 
 exports.requestHandler = async function(event, context) {
     // console.log('Received event:', JSON.stringify(event, null, 2));
-    const bodyObj = parseBody(event.body, event.isBase64Encoded);
+    const bodyObj = parseBody(event.body);
 
     // first check that the api-key passed in the header matches the environment variable api-key
 
@@ -74,7 +101,7 @@ exports.requestHandler = async function(event, context) {
     const requestMongoDBResult = await requestMongoDBPostRequest(process.env.MONGODB_NAME, bodyObj)
         .then(responseData => {
             console.log('Response data:', responseData);
-            return { type: 'success', response: responseData };
+            return { type: 'success', result: parseBody(responseData) };
         })
         .catch(err => {
             // console.error('Error:', err);
