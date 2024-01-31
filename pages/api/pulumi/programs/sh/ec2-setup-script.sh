06ec6212744afe854087f71524704cbd43bfc271
Joshua Mbogo <55801923+mbogo-mit@users.noreply.github.com>
2024-01-31 14:19:56 -0500

    Update ec2-setup-script.sh

--- pages/api/pulumi/programs/sh/ec2-setup-script.sh
+++ pages/api/pulumi/programs/sh/ec2-setup-script.sh
@@ -4,22 +4,6 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 yum -y update
 echo "Finish basic update and system log config"
 
-# Default Mintlify Values
-MINTLIFY_NAME="Mintlify-API-Kit"
-MINTLIFY_REPO_URL="https://github.com/mbogo-mit/mintlify-starter-kit.git"
-
-# Echo variable names
-echo "MINTLIFY_NAME: $MINTLIFY_NAME"
-echo "MINTLIFY_REPO_URL: $MINTLIFY_REPO_URL"
-
-# Default GraphQL API Values
-GQL_NAME="GraphQL-PE-CORE-API"
-GQL_REPO_URL="https://github.com/webhubhq/GraphQL-PE-CORE-API.git"
-
-# Echo variable names
-echo "GQL_NAME: $GQL_NAME"
-echo "GQL_REPO_URL: $GQL_REPO_URL"
-
 # Default Node Server Values
 NODE_NAME="Node-Server-API"
 NODE_REPO_URL="https://github.com/mbogo-mit/node-express-boilerplate.git"
@@ -71,22 +55,6 @@ server {
 
     location / {
         rewrite ^\/(.*)$ /$1 break;
-        proxy_pass http://localhost:3000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    }
-
-    location /api/gql {
-        rewrite ^\/api\/gql\/(.*)$ /api/gql/$1 break;
-        proxy_pass http://localhost:4000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    }
-
-    location /api/node {
-        rewrite ^\/api\/node\/(.*)$ /api/node/$1 break;
         proxy_pass http://localhost:5000;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
@@ -102,26 +70,6 @@ EndOfText
 sudo systemctl reload nginx
 
 echo "Reloaded NGINX with api.conf"
-
-
-
-echo "Initializing $GQL_NAME"
-
-# Create a directory for your GQL App
-sudo mkdir -p "/var/www/$GQL_NAME"
-# sudo chown -R ec2-user:ec2-user "/var/www/$GQL_NAME"
-
-# Navigate to the app directory
-cd "/var/www/$GQL_NAME"
-
-# Clone your app repository
-git clone "$GQL_REPO_URL" .
-
-# Install app dependencies
-npm install
-
-# start the Apollo Server built in repo
-(npm run start)
 
 
 echo "Initializing $NODE_NAME"
@@ -141,25 +89,3 @@ npm install
 
 # start the Node Server built in repo
 (npm run start)
-
-
-
-echo "Initializing $MINTLIFY_NAME"
-
-# Create a directory for your app
-sudo mkdir -p "/var/www/$MINTLIFY_NAME"
-# sudo chown -R ec2-user:ec2-user "/var/www/$MINTLIFY_NAME"
-
-# Navigate to the app directory
-cd "/var/www/$MINTLIFY_NAME"
-
-# Clone your app repository
-git clone "$MINTLIFY_REPO_URL" .
-
-# Install app dependencies
-npm install -g mintlify
-
-mintlify install
-
-# start the Mintlify App
-(mintlify dev)
